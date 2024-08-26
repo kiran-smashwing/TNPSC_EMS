d56a6b6972e6752722d7b8c3590da9f536365ea0
Kiran <119150678+kiranCodings@users.noreply.github.com>
2024-08-26 15:49:04 +0530

    feat: Refactor additional candidate modals and views
    - Removed unnecessary button from additional-candidate.blade.php
    - Updated layout and formatting in additional-candidate-view.blade.php

--- resources/views/modals/additional-candidate.blade.php
+++ resources/views/modals/additional-candidate.blade.php
@@ -19,7 +19,6 @@
                         <label for="candidateName" class="form-label">Name:</label>
                         <input type="text" class="form-control" id="candidateName" placeholder="Enter Candidate Name">
                     </div>
-                    <button type="button" class="btn btn-primary" onclick="addCandidate()">Add Candidate</button>
                 </form>
                
             </div>

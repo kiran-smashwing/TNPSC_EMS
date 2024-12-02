aead219cb8d40ca1fdfc300abd57ecec5dd10eea
MsathishRS <sathishm@smashwing.com>
2024-12-02 12:06:51 +0530

    email temp

--- resources/views/email/center_created.blade.php
+++ resources/views/email/center_created.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>உங்கள் கணக்கு வெற்றிகரமாக உருவாக்கப்பட்டது.</title>
+    <title>மறந்துபோன கடவுச்சொல்</title>
     <style>
         body {
             font-family: 'Arial', sans-serif;
@@ -71,30 +71,13 @@
             <h2>வணக்கம் {{ $name }},</h2>
             <p>உங்கள் கணக்கு வெற்றிகரமாக உருவாக்கப்பட்டது. தமிழ்நாடு அரசு பணியாளர் தேர்வாணையத்திற்கு உங்கள் சேவையை
                 சிறந்த முறையில் வழங்க கேட்டுக்கொள்கிறோம்</p>
-
-            <p>கீழே உங்கள் உள்ளீட்டு கணக்கு விவரங்கள்:</p>
-            <ul>
-                <strong>மின்னஞ்சல்: </strong>{{ $email }}
-                <strong>கடவுச்சொல்:</strong>{{ $password }}
-            </ul>
-
-            <p>தயவுசெய்து உங்கள் கணக்கு விவரங்களை சரிபார்க்கவும். ஏதேனும் தவறு இருந்தால், உடனடியாக புதுப்பிக்கவும்.</p>
-
-            <p>உங்கள் கணக்கைப் பயன்படுத்துவது எப்படி என்கிற வழிகாட்டும் காணொளி <strong>என் சுயவிவரப் பக்கம்</strong> (My
-                Profile Page) பகுதியில் கிடைக்கிறது.</p>
-
-            <h3>TNPSC EMS செயலி பதிவிறக்க:</h3>
-            <p>TNPSC EMS செயலியை Google Play Store-இல் பதிவிறக்கம் செய்யவும்:</p>
-            <a href="{{ $playstore_link }}">TNPSC EMS செயலி பதிவிறக்கம் செய்ய இங்கே கிளிக் செய்யவும்</a>
-
-            <h3>பொது அறிவுறுத்தல்கள்:</h3>
-            <ul>
-                <li>சரியான நேரத்தில் பரிசோதனையை தொடங்கவும்.</li>
-                <li>பரிசோதனையின் அனைத்து சாகசங்களை முழுமையாக கண்காணிக்கவும்.</li>
-                <li>எந்த தொழில்நுட்ப பிரச்சனைகளும் ஏற்படாதபடி உறுதிப்படுத்தவும்.</li>
-            </ul>
+                <p>கீழே உங்கள் உள்ளீட்டு கணக்கு விவரங்கள்:</p>
+                <ul>
+                    <strong>மின்னஞ்சல்: </strong>{{ $email }}
+                    <strong>கடவுச்சொல்:</strong>{{ $password }}
+                </ul>
+            <a href="{{ url('/login') }}" class="button">உள்நுழைக</a>
         </div>
-
         <div>
             <p>நன்றி,</p>
             <p>உங்கள் <strong>சேவை குழு</strong></p>
@@ -102,13 +85,10 @@
         </div>
         <!-- Footer -->
         <div class="footer">
-            <p>Copyright © {{ date('Y') }} TNPSC. Developed By Smashwing Technologies Pvt Ltd. All rights reserved.
-            </p>
+            <p>Copyright © {{ date('Y') }} TNPSC. Developed By Smashwing Technologies Pvt Ltd. All rights reserved.</p>
 
         </div>
     </div>
 </body>
 
 </html>
-
-
